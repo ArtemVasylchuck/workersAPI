d535b8ea0e78fa4a897858ff7d5f14703f0abe35
ArtemVasylchuck <artem.vasylchuck@gmail.com>
2023-10-23 21:38:09 +0300

    CORS headers add

--- workersAPI/settings.py
+++ workersAPI/settings.py
@@ -15,6 +15,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -28,12 +32,15 @@ INSTALLED_APPS = [
 
     'rest_framework',
 
+    'corsheaders',
+
     'workers_api.apps.WorkersApiConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
